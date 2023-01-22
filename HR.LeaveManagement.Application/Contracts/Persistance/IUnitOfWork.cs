86bffabab6c2a5dbee428a601c0571555e775b82
ShaikZuhairAhamedd <zuhairshaik327@gmail.com>
2023-01-22 21:29:34 +0530

    Added UnitOfWork Interface

---
+++ HR.LeaveManagement.Application/Contracts/Persistance/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using HR.LeaveManagement.Application.Contracts.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Contracts.Persistance
+{
+    public interface IUnitOfWork:IDisposable
+    {
+        ILeaveTypeRepository leaveTypeRepository { get; }
+        ILeaveRequestRepository leaveRequestRepository { get; }
+        ILeaveAllocationRepository leaveAllocationRepository { get; }
+        Task Save();
+    }
+}
